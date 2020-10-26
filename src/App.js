ba12233ffde546d11a7181418da6bde3ab185827
Nasita Haque <nhaque@fandm.edu>
2020-10-25 21:21:52 -0400

    updated App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Footer from './components/Footer';
 import GreenEco from './components/GreenEco';
 import Travlr from './components/Travlr';
 import Covered from './components/Covered';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -30,3 +31,25 @@ function App() {
 };
 
 export default App;
+
+
+// function App() {
+//   return (
+//     <Router>
+//     <div className="page">
+//       <Navbar />
+//         <Switch>
+//           <Route exact path="/" component={Home} />
+//           <Route exact path="/projects" component={Projects} />
+//           <Route exact path="/aboutme" component={AboutMe} />
+//           <Route exact path="/greeneco" component={GreenEco} />
+//           <Route exact path="/travlr" component={Travlr} />
+//           <Route exact path="/covered" component={Covered} />
+//         </Switch>
+//     </div>
+//     <Footer />
+//     </Router>
+//   )
+// };
+
+// export default App;
