8bc86485b4a5a28a071acb06bd075ae065a46ed9
Nasita Haque <nhaque@fandm.edu>
2020-10-25 22:37:59 -0400

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Footer from './components/Footer';
 import GreenEco from './components/GreenEco';
 import Travlr from './components/Travlr';
 import Covered from './components/Covered';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -20,9 +19,9 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/projects" exact component={Projects} />
           <Route path="/aboutme" exact component={AboutMe} />
-          <Route path="/greeneco" component={GreenEco} />
-          <Route path="/travlr" exact component={Travlr} />
-          <Route path="/covered" exact component={Covered} />
+          <Route path="greeneco" component={GreenEco} />
+          <Route path="travlr" exact component={Travlr} />
+          <Route path="covered" exact component={Covered} />
         </Switch>
     </div>
     <Footer />
@@ -31,25 +30,3 @@ function App() {
 };
 
 export default App;
-
-
-// function App() {
-//   return (
-//     <Router>
-//     <div className="page">
-//       <Navbar />
-//         <Switch>
-//           <Route exact path="/" component={Home} />
-//           <Route exact path="/projects" component={Projects} />
-//           <Route exact path="/aboutme" component={AboutMe} />
-//           <Route exact path="/greeneco" component={GreenEco} />
-//           <Route exact path="/travlr" component={Travlr} />
-//           <Route exact path="/covered" component={Covered} />
-//         </Switch>
-//     </div>
-//     <Footer />
-//     </Router>
-//   )
-// };
-
-// export default App;
