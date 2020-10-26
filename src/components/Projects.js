1be5bf69be81073565ef6ef041b36cd28079c005
Nasita Haque <nhaque@fandm.edu>
2020-10-25 21:35:52 -0400

    update projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,13 +7,13 @@ import coveredthumbnail from "../images/covered-thumbnail.png";
 function Projects (){
 	return (
 		<div className="project-container">
-			<a href="/#/greeneco"> <img src={greenecothumbnail} alt="Green Eco project" /> 
+			<a href="#greeneco"> <img src={greenecothumbnail} alt="Green Eco project" /> 
 				<div className="title"> GREEN ECO</div>  
 			</a>
-			<a href="/#/travlr"> <img src={travlrthumbnail} alt="TRAVLR project" />
+			<a href="#travlr"> <img src={travlrthumbnail} alt="TRAVLR project" />
 				<div className="title">TRAVLR</div> 
 			</a>
-			<a href="/#/covered"> <img src={coveredthumbnail} alt="Covered project" />
+			<a href="#covered"> <img src={coveredthumbnail} alt="Covered project" />
 				<div className="title"> COVERED</div>
 			</a>
 		</div>
